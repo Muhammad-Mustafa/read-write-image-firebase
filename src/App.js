ba71326dd8e67ed02af084f1617c5f2756e0cb46
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2020-04-08 00:21:15 +0500

    All the bugs are Removed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Webcam from "react-webcam";
+import "./style.css";
+import * as firebase from "firebase";
 
-function App() {
+export default function App() {
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  };
+  var imgCount = "";
+  var imgURL = "";
+
+  const webcamRef = React.useRef(null);
+
+  async function getImageCounter() {
+    console.log("Start 1");
+
+    await firebase
+      .database()
+      .ref("Counter/")
+      .once("value")
+      .then(function(snapshot) {
+        imgCount = snapshot.val().Count;
+        // console.log("snapshot.val().Count  " + snapshot.val().Count);
+        console.log("f1");
+      });
+  }
+
+   async function uploadImage(imageSrc) {
+    console.log("Start 2");
+    var uploadTask = firebase.storage().ref();
+    await uploadTask
+      .child("pic" + imgCount)
+      .putString(imageSrc, "data_url")
+      .then(function(snapshot) {
+        // console.log("Uploaded a data_url string!");
+        console.log("f2");
+      });
+  }
+
+   async function getURL() {
+    console.log("Start 3");
+    var url = firebase.storage().ref();
+
+    await url
+      .child("pic" + imgCount)
+      .getDownloadURL()
+      .then(function(downloadURL) {
+        imgURL = downloadURL;
+        console.log("File available at", imgURL);
+        console.log("f3");
+      });
+  }
+
+   function updateCounter(){
+    console.log("Start 4");
+      imgCount++;
+      firebase.database().ref('Counter/').set({
+        Count: imgCount
+      });
+      console.log("f4");
+  }
+
+  async function capture () {
+    // this will get the base64 code of the image
+    const imageSrc = webcamRef.current.getScreenshot();
+    await getImageCounter();
+    await uploadImage(imageSrc);
+    await getURL();
+    await updateCounter();
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="webcam">
+        <Webcam
+          className="webcam"
+          audio={false}
+          ref={webcamRef}
+          mirrored={true}
+          screenshotQuality={1}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+        />
+      </div>
+      <button onClick={capture}>Capture photo</button>
+    </>
   );
 }
-
-export default App;
+// }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBmwjNr9pzagjCAcs4_L0-ttVu6far6CAI",
+  authDomain: "read-write-pictures.firebaseapp.com",
+  databaseURL: "https://read-write-pictures.firebaseio.com",
+  projectId: "read-write-pictures",
+  storageBucket: "read-write-pictures.appspot.com",
+  messagingSenderId: "869602270138",
+  appId: "1:869602270138:web:481ed323b67d39c0191b83"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
